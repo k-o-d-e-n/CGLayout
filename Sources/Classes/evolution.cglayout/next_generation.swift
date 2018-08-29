eca73c2ca851e18caf7fe6308c937840674d5271
Denis Koryttsev <koden.u8800@gmail.com>
2018-08-29 15:37:34 +0300

    Made freeAreaLayoutGuide in AutoLayoutViewController as analogue safe area.

--- Sources/Classes/evolution.cglayout/next_generation.swift
+++ Sources/Classes/evolution.cglayout/next_generation.swift
@@ -20,7 +20,7 @@ public class LayoutManager<Item: LayoutElement>: NSObject {
     var deinitialization: (() -> Void)?
     weak var item: LayoutElement!
     var scheme: LayoutScheme!
-    private(set) var isNeedLayout: Bool = false
+    private var isNeedLayout: Bool = false
 
     public func setNeedsLayout() {
         if !isNeedLayout {
@@ -66,13 +66,11 @@ public extension LayoutManager where Item: UIView {
 open class AutolayoutViewController: UIViewController {
     fileprivate lazy var internalLayout: LayoutScheme = self.loadInternalLayout()
     public lazy internal(set) var layoutScheme: LayoutScheme = self.loadLayout()
-    public var upperLayoutGuide = LayoutGuide<UIView>(frame: .zero)
-    public var lowerLayoutGuide = LayoutGuide<UIView>(frame: .zero)
+    public lazy var freeAreaLayoutGuide = LayoutGuide<UIView>()
 
     override open func viewDidLoad() {
         super.viewDidLoad()
-        view.add(layoutGuide: upperLayoutGuide)
-        view.add(layoutGuide: lowerLayoutGuide)
+        view.add(layoutGuide: freeAreaLayoutGuide)
     }
 
     override open func viewDidLayoutSubviews() {
@@ -98,23 +96,49 @@ open class AutolayoutViewController: UIViewController {
     }
 
     fileprivate func loadInternalLayout() -> LayoutScheme {
-        let visible: (CGRect) -> CGRect = { [unowned self] rect in
+        let visible: (inout CGRect) -> Void = { [unowned self] rect in
             if #available(iOS 11.0, tvOS 11.0, *) {
-                return UIEdgeInsetsInsetRect(rect, self.view.safeAreaInsets)
+                rect = UIEdgeInsetsInsetRect(rect, self.view.safeAreaInsets)
             } else {
-                return UIEdgeInsetsInsetRect(rect, self.view.layoutMargins)
+                rect = UIEdgeInsetsInsetRect(rect, self.viewContentInsets)
             }
         }
-        return LayoutScheme(blocks: [
-            upperLayoutGuide.layoutBlock(
-                with: Layout(x: .equal, y: .equal, width: .equal, height: .fixed(0)),
-                constraints: [AnonymConstraint(anchors: [Top.limit(on: .inner)], constrainRect: visible)]
-            ),
-            lowerLayoutGuide.layoutBlock(
-                with: Layout(x: .equal, y: .bottom(), width: .equal, height: .fixed(0)),
-                constraints: [AnonymConstraint(anchors: [Bottom.limit(on: .inner)], constrainRect: visible)]
-            )
-        ])
+        return LayoutScheme(
+            blocks: [freeAreaLayoutGuide.layoutBlock(constraints: [AnonymConstraint(transform: visible)])]
+        )
+    }
+
+    @available(iOS 9.0, *)
+    private var viewContentInsets: UIEdgeInsets {
+        let bars = heightBars
+        return UIEdgeInsets(top: bars.top,
+                            left: 0,
+                            bottom: bars.bottom,
+                            right: 0)
+    }
+
+    @available(iOS 9.0, *)
+    private var heightBars: (top: CGFloat, bottom: CGFloat) {
+        guard let window = UIApplication.shared.delegate.flatMap({ $0.window }).flatMap({ $0 }), let superview = viewIfLoaded?.superview else {
+            return (UIApplication.shared.statusBarFrame.height + (navigationController.map { $0.isNavigationBarHidden ? 0 : $0.navigationBar.frame.height } ?? 0),
+                    tabBarController.map { $0.tabBar.isHidden ? 0 : $0.tabBar.frame.height } ?? 0)
+        }
+
+        var topFrame = window.convert(UIApplication.shared.statusBarFrame, to: superview)
+        topFrame = topFrame.union(navigationController.map { contr -> CGRect in
+            contr.isNavigationBarHidden ?
+                .zero :
+                superview.convert(contr.navigationBar.frame, from: contr.navigationBar.superview)
+            } ?? .zero)
+
+        let bottomBarsTop = tabBarController.map { contr -> CGPoint in
+            contr.tabBar.isHidden ?
+                .zero :
+                superview.convert(contr.tabBar.frame.origin, from: contr.tabBar.superview)
+        }
+
+        return (max(0, topFrame.maxY - view.frame.origin.y),
+                max(0, bottomBarsTop.map { $0.y - view.frame.maxY } ?? 0))
     }
 }
 
